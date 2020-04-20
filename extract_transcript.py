f45dfe0a218f8a88bf55feecb77de21c7b0388e2
Anna Korsakova <leseyras@gmail.com>
2020-04-20 23:06:31 +0800

    added extract transcript and train files

---
+++ extract_transcript.py
@@ -0,0 +1,105 @@
+import numpy as np
+from Bio import SeqIO
+import random
+import os
+import sys
+
+def complementary(let):
+	#A-T, C-G
+	if let=='A':
+		return 'T'
+	if let=='T':
+		return 'A'
+	if let=='C':
+		return 'G'
+	if let=='G':
+		return 'C'
+
+def hot_encode_seq(let):
+	#hot-encode the sequence, where "O" corresponds to zero-padded areas
+    if let=='A':
+        return([1,0,0,0])
+    elif let=='T':
+        return([0,1,0,0])
+    elif let=='C':
+        return([0,0,1,0])
+    elif let=='G':
+        return([0,0,0,1])
+    elif let=='O':
+        return([0,0,0,0])
+
+def hot_encode_label(let):
+	#hot-encode the labels, where "p" corresponds to zero-padded areas
+    if let=='p':
+        return([0,0,0])
+    elif let=='b':
+        return([1,0,0])
+    elif let=='a':
+        return([0,1,0])
+    elif let=='d':
+        return([0,0,1])
+
+def zero_pad(seq):
+	if isinstance(seq, str):
+		pad = 5000 - (len(seq)-2000)%5000
+		seq_ = (pad//2)*'O' + seq + (pad - pad//2)*'O'
+	return seq_
+
+#extracting the transcripts for just one chromosome
+fasta_seq = SeqIO.parse(open('./data/chr20.fa'), 'fasta')
+
+for fasta in fasta_seq:
+	name, sequence = fasta.id, str(fasta.seq)
+
+#file with principal transcripts from GENCODE v33
+transcript_file = np.genfromtxt('./data/GENCODE_v33_basic', usecols=(2,3,4,5), dtype='str')
+
+transcripts = []
+labels = []
+
+for row in transcript_file:
+	if row[0]=='chr20':
+		# adding the transcripts of the sense strand: whole transcript + flanks + zero-padded, labels + zero-padded
+		if row[1]=='+':
+			# extract the transcript sequence with 1k flanks
+			s = sequence[int(row[2])-1000 : int(row[3])+1000].upper()
+			if 'N' not in s:
+				# padding labels here 
+				pad = 5000 - (len(s)-2000)%5000
+				y = (pad//2)*'p' + 'a' + (len(s)-2002)*'b' + 'd' + (pad - pad//2)*'p'
+				labels.append(y)
+				# padding sequence with Os
+				s = zero_pad(s)
+				transcripts.append(s)
+		# adding the transcripts of the antisense strand
+		if row[1]=='-':
+			s = sequence[int(row[2])-1000 : int(row[3])+1000].upper()
+			if 'N' not in s:
+				# padding labels here 
+				pad = 5000 - (len(s)-2000)%5000
+				y = (pad//2)*'p' + 'a' + (len(s)-2002)*'b' + 'd' + (pad - pad//2)*'p'
+				# hot-encoding labels and adding hot-encoded labels to a new list
+				labels.append(y)
+				# getting complementary seq
+				s = ''.join([complementary(x) for x in s])
+				# padding sequence with Os
+				s = zero_pad(s)
+				transcripts.append(s)
+
+print("GENCODE_v33_basic transcripts for a given chromosome: {}".format(len(transcripts)))
+
+#cut these sequences and labels into 5000 chunks
+transcripts_ = []
+labels_ = []
+
+#transform into chunks 
+for i in range(len(transcripts)):
+	chunks = (len(transcripts[i])-2000)//5000
+	for j in range(1, chunks+1):
+		s = transcripts[i]
+		l = labels[i]
+		transcripts_.append(s[5000*(j-1) : 5000*j+2000])
+		labels_.append(l[5000*(j-1) : 5000*j])
+
+np.savetxt('./data/transcripts_chunks_chr10', transcripts_, fmt='%s', delimiter='\t')
+np.savetxt('./data/labels_chunks_chr10', labels_, fmt='%s', delimiter='\t')
---
+++ train_spliceAI2k.py
@@ -0,0 +1,206 @@
+from __future__ import print_function
+import keras
+from keras.layers import Dense, Conv1D, BatchNormalization, Activation, Dropout
+from keras.layers import AveragePooling2D, Input, Flatten, MaxPooling2D, Cropping1D
+from keras.optimizers import Adam
+from keras.callbacks import ModelCheckpoint, LearningRateScheduler
+from keras.callbacks import ReduceLROnPlateau
+from keras.preprocessing.image import ImageDataGenerator
+from keras.regularizers import l2
+from keras import backend as K
+from keras.models import Model, Sequential
+from keras.utils import to_categorical, plot_model
+from keras.datasets import cifar10
+from keras import optimizers
+
+from sklearn.preprocessing import StandardScaler
+from sklearn.model_selection import train_test_split
+from sklearn.metrics import confusion_matrix, classification_report
+
+import numpy as np
+import os
+
+
+def hot_encode_seq(let):
+    #hot-encode the sequence, where "O" corresponds to zero-padded areas
+    if let=='A':
+        return([1,0,0,0])
+    elif let=='T':
+        return([0,1,0,0])
+    elif let=='C':
+        return([0,0,1,0])
+    elif let=='G':
+        return([0,0,0,1])
+    elif let=='O':
+        return([0,0,0,0])
+
+def hot_encode_label(let):
+    #hot-encode the labels, where "p" corresponds to zero-padded areas
+    if let=='p':
+        return([0,0,0])
+    elif let=='b':
+        return([1,0,0])
+    elif let=='a':
+        return([0,1,0])
+    elif let=='d':
+        return([0,0,1])
+
+
+def lr_schedule(epoch):
+    #learning rate scheduler
+    lr = 0.001
+    if epoch == 7:
+        lr *= 0.5
+    elif epoch == 8:
+        lr *= (0.5)**2
+    elif epoch == 9:
+        lr *= (0.5)**3
+    elif epoch == 10:
+        lr *= (0.5)**4
+    print('Learning rate: ', lr)
+    return lr
+
+# TRAINING PARAMETERS
+batch_size = 12
+num_classes = 3
+epochs = 10
+data_augmentation = False
+
+def RB_block(inputs,
+             num_filters=32,
+             kernel_size=11,
+             strides=1,
+             activation='relu',
+             dilation_rate=1):
+    """1D Convolution-Batch Normalization-Activation stack builder
+
+    # Arguments
+        inputs (tensor): input tensor from input image or previous layer
+        num_filters (int): Conv2D number of filters
+        kernel_size (int): Conv2D square kernel dimensions
+        strides (int): Conv2D square stride dimensions
+        activation (string): activation name
+        dilation rate (int): dilation rate
+
+    # Returns
+        x (tensor): tensor as input to the next layer
+    """
+    conv = Conv1D(num_filters,
+                  kernel_size=kernel_size,
+                  strides=strides,
+                  padding='same',
+                  dilation_rate=dilation_rate)
+
+    x = inputs
+
+    for layer in range(2):
+        x = BatchNormalization()(x)
+        x = Activation(activation)(x)
+        x = conv(x)
+
+    return x
+
+
+def spliceAI_model(input_shape, num_classes=3):
+    """Model builder
+
+    Shortcut layers after every 4 RB blocks.
+
+    # Arguments
+        input_shape (tensor): shape of input image tensor
+        num_classes (int): number of classes
+
+    # Returns
+        model (Model): Keras model instance
+    """
+
+    inputs = Input(shape=input_shape)
+
+    # initiate 
+    x = Conv1D(32, kernel_size=1, strides=1, padding='same', dilation_rate=1)(inputs)
+    y = Conv1D(32, kernel_size=1, strides=1, padding='same', dilation_rate=1)(inputs)
+
+    # RB 1: 32 11 1
+    for stack in range(4):
+        x = RB_block(x, num_filters=32, kernel_size=11, strides=1, activation='relu', dilation_rate=1)
+
+    y = keras.layers.add([x, y])
+
+    # RB 2: 32 11 4
+    for stack in range(4):
+        x = RB_block(x, num_filters=32, kernel_size=11, strides=1, activation='relu', dilation_rate=4)
+
+    y = keras.layers.add([x, y])  
+
+    # RB 3: 32 21 10
+    for stack in range(4):
+        x = RB_block(x, num_filters=32, kernel_size=21, strides=1, activation='relu', dilation_rate=10)
+
+    x = Conv1D(32, kernel_size=1, strides=1, padding='same', dilation_rate=1)(x)
+
+    # now adding up what was shortcut from the prev layers
+    x = keras.layers.add([x, y]) 
+
+    x = Conv1D(3, kernel_size=1, strides=1, padding='same', dilation_rate=1)(x)
+
+    x = Dense(num_classes, activation='softmax')(x)
+
+    outputs = Cropping1D(cropping=(1000, 1000))(x)
+
+    model = Model(inputs=inputs, outputs=outputs)
+
+    return model
+
+
+transcripts_ = np.loadtxt('./data/transcripts_chunks_chr20', dtype='str', delimiter='\t')
+labels_ = np.loadtxt('./data/labels_chunks_chr20', dtype='str', delimiter='\t')
+
+transcripts = []
+labels = []
+
+# hot-encode
+for i in range(len(transcripts_)):
+    # hot-encode seq
+    transcripts.append([np.array(hot_encode_seq(let)) for let in transcripts_[i]])
+    # hot-encode labels
+    labels.append([np.array(hot_encode_label(x)) for x in labels_[i]])
+
+transcripts = np.array(transcripts)
+labels = np.array(labels)
+
+(x_train, x_test, y_train, y_test) = train_test_split(transcripts,
+    labels, test_size=0.2)
+
+input_shape = x_train.shape[1:]
+
+print('x_train shape:', x_train.shape)
+print(x_train.shape[0], 'train samples')
+print(x_test.shape[0], 'test samples')
+print('y_train shape:', y_train.shape)
+
+lr_scheduler = LearningRateScheduler(lr_schedule)
+
+model = spliceAI_model(input_shape=input_shape)
+
+model.compile(loss='categorical_crossentropy',
+              optimizer=Adam(learning_rate=lr_schedule(0)),
+              metrics=['accuracy'])
+
+print(model.summary())
+
+model.fit(x_train, y_train, batch_size=batch_size, epochs=epochs, callbacks=[lr_scheduler], validation_data=(x_test, y_test), shuffle=True)
+
+model.save('./data/model_spliceAI2k')
+
+scores = model.evaluate(x_test, y_test, verbose=1)
+print('Test loss:', scores[0])
+print('Test accuracy:', scores[1])
+
+predictions = model.predict(x_test)
+
+print(predictions)
+
+print(classification_report(y_test.argmax(axis=1),
+    predictions.argmax(axis=1), target_names=['Blank', 'Donor', 'Acceptor']))
+
+print(confusion_matrix(y_test.argmax(axis=1), predictions.argmax(axis=1)))
